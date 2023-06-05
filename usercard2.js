8faa6b25f18f8c26ebc84a1224b6c78f40b88d9c
Harshitha-31 <134955516+Harshitha-31@users.noreply.github.com>
2023-06-05 22:28:05 +0530

    changed

---
+++ usercard2.js
@@ -0,0 +1,25 @@
+let MSDhoniinfo={
+    imgurl: "https://cdn.britannica.com/25/222725-050-170F622A/Indian-cricketer-Mahendra-Singh-Dhoni-2011.jpg",
+    name: "M.S.Dhoni",
+    Description: "Cricket Player"
+}
+let dhoniyounginfo={
+    imgurl: "https://img.mensxp.com/media/content/2021/Jun/5b990658b0abcb6de15be4b61d42d8c3_60d182271438b.jpeg",
+    name: "Mahendra Singh Dhoni",
+    Description:"A famous Cricket player"
+}
+let isdhoniold=true;
+let displayobject;
+let flipdata=function(){
+    if (isdhoniold ==true){
+        displayobject=dhoniyounginfo;
+        isdhoniold=false;
+    }
+    else{
+        displayobject=dhonioldinfo;
+        isdhonild=true;
+    }
+    document.getElementById("dhoni-img").src=displayobject.imgurl;
+    document.getElementById("dhoni-name").innerHTML= displayobject.name;
+    document.getElementById("dhoni-description").innerHTML=displayobject.description;
+}
